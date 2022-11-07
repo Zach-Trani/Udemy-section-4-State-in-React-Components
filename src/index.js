8432db96bf37b9b4ede562c6e0c51371c8dbfca7
Zach-Trani <trani.zach@gmail.com>
2022-11-06 19:54:01 -0500

    added conditional rendering in response to user's choice on sharing location

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// class based component
+// class based component - this is called two times (returns some JSX, and a second time when updating state).
 class App extends React.Component {
-    // executed first - will always write constructor + super methods
+    // executed first - initial setup when component first created, must call super.
     constructor(props) {
         super(props);
 
         // Only time we do direct assignment to this.state!
-        this.state = { lat: null }; // This is the React State - will be updated (number unknown initially)
+        this.state = { lat: null, errorMessage: '' }; // This is the React State - will be added (number unknown initially), and not deleted.
 
         window.navigator.geolocation.getCurrentPosition(
             // success callback function
             (position) => {
-                // to update state object - we called setState!
-                this.setState({ lat: position.coords.latitude })
+                // to update state object - we called setState! This does not run when we are running the constructor - runs at some point in future.
+                this.setState({ lat: position.coords.latitude }) // this is shown in the browser console
             },
-            (err) => console.log(err) // err
+            (err) => {
+                this.setState({ errorMessage: err.message }); // this is shown in the browser console
+            }
         );
     }
 
-    // React says we have to define render! - this is being called frequently
-    render () {
-        return <div>Latitude: {this.state.lat}</div>;
+    // React says we have to define render! - this is being called frequently. Conditional rendering in this case.
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>
     }
 }
 
